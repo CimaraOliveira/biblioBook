b71fe1ec2d16bd4843cf1a8b9ce1665e33a250c9
CimaraOliveira <35199183+CimaraOliveira@users.noreply.github.com>
2025-04-30 18:41:10 -0300

    adicionando models livro e reserva

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Livro, Reserva
 
-# Register your models here.
+admin.site.register(Livro)
+admin.site.register(Reserva)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.CharField(max_length=200)
+    quantidade = models.PositiveIntegerField(default=1)
+    isbn = models.IntegerField(max_length=20)
+    ano_publicação = models.CharField(max_length=200)
+
+
+class Reserva(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
+    data_reserva = models.DateTimeField(auto_now_add=True)
+    data_retirada = models.DateField(null=True, blank=True)
+    data_devolucao = models.DateField(null=True, blank=True)
+    ativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.usuario.username} reservou {self.livro.titulo}"
